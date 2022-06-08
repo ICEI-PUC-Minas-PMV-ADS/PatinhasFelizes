6d1f81707a62f5d474672aa601b80cf4edaeb7ff
Jade <jadepersiano@hotmail.com>
2022-06-07 21:39:03 -0300

    Inseri uma nova página de js com a validação de login e estou fazendo a parte do json

---
+++ src/js/login.js
@@ -0,0 +1,45 @@
+
+function logartest () {
+    var login = document.getElementById("email").value;
+    var senha = document.getElementById("senha").value;
+    var validar = 0
+
+    for (i=0; i < usuarios.dados.length; i++) {
+        if (login == usuarios.dados[i].email && senha == usuarios.dados[i].senha){
+            validar = 1
+            alert("Sucesso");
+            location.href = ""
+            break
+        }
+
+    }
+    if (validar == 0) {
+        alert("Usuário ou senha incorretos!");
+    }
+}
+
+var usuarios = {
+    dados: [
+    {
+      "email": "jadepersiano@hotmail.com",
+      "senha": "12345678"
+    },
+    {
+        "email": "maria123@gmail.com",
+        "senha": "xuxu4321"
+    },
+    {
+        "email": "amocachorro@hotmail.com",
+        "senha": "pets122"
+    },
+    {
+        "email": "pcunha@outlook.com",
+        "senha": "paulocunha1"
+    },
+    {
+        "email": "ongfocinhodeluz@hotmail.com",
+        "senha": "focinhoss00"
+    }
+]
+}
+  
